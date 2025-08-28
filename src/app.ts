d3a019368b917bb19d9e9ac0612438af4c85860c
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-08-27 23:12:02 -0600

    feat: implement bot start and help commands for Telegram bot

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,9 @@
+import { bot } from "@/lib/telegraf-bot";
 
-console.log("Hello, world!");
+bot.start((ctx) => {
+  ctx.reply("Bienvenido! Soy un bot de Telegram y mi tarea es ayudarte a descargar archivos multimedia desde las redes sociales que desees, pero espera, no tenemos soporte para todas las redes sociales asi que si quieres saber que redes sociales tenemos soporte, usa /support.")
+});
+
+bot.help((ctx) => {
+  ctx.reply("Aquí tienes una lista de comandos que puedes usar:\n/start - Iniciar el bot\n/help - Obtener ayuda");
+});
