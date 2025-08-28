6db105eba73c427579c87399c81e06d0b6434ebb
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-08-28 00:07:21 -0600

    feat: add bot launch and signal handling for graceful shutdown

--- src/app.ts
+++ src/app.ts
@@ -10,3 +10,7 @@ Object.entries(commands).forEach(([command, { action }]) => {
   bot.command(command, action);
 });
 
+bot.launch();
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
