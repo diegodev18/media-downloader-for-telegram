ac2667ab0ce7149d62b90fccd6d67ad9fe58a711
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-08-28 00:35:40 -0600

    fix: update import path for commands in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import { bot } from "@/lib/telegraf-bot";
 import { message } from "telegraf/filters";
 import { FormBot } from "@/utils/form-bot";
-import { commands } from "./consts/commands";
+import { commands } from "@/consts/commands";
 
 bot.start(FormBot.start);
 
