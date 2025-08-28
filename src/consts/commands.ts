8aaa25dd757e66f6bacbf77b02722359d7fc25bd
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-08-27 23:22:32 -0600

    feat: remove help command from commands list

--- src/consts/commands.ts
+++ src/consts/commands.ts
@@ -1,10 +1,6 @@
 import { FormBot } from "@/utils/form_bot";
 
 export const commands = {
-  help: {
-    description: "Obtener ayuda",
-    action: FormBot.help,
-  },
   support: {
     description: "Ver redes sociales soportadas",
     action: FormBot.support,
