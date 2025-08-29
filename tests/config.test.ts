de5be062dfeda477c03188e6218a7707eb23bded
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-08-29 16:49:51 -0600

    feat: add initial environment configuration and tests for required variables

---
+++ tests/config.test.ts
@@ -0,0 +1,22 @@
+if (
+  process.env.NODE_ENV !== 'production' && process.loadEnvFile
+) {
+  process.loadEnvFile(".env");
+}
+
+describe('Valid required configs', () => {
+  test('should have some env variables', () => {
+    expect(process.env.BOT_TOKEN).toBeDefined();
+    expect(process.env.COOKIES).toBeDefined();
+  })
+
+  test('should have a valid BOT_TOKEN', () => {
+    const { BOT_TOKEN } = process.env;
+
+    fetch(`https://api.telegram.org/bot${BOT_TOKEN}/getUpdates`)
+      .then(res => res.json())
+      .then(data => {
+        expect(data.ok).toBe(true);
+      });
+  })
+})
