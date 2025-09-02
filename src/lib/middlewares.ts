96b2f1d4eada4c8c397600bc0243c1aed8092043
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-09-01 19:28:29 -0600

    fix: correct command slicing and log output format in logRequest middleware

--- src/lib/middlewares.ts
+++ src/lib/middlewares.ts
@@ -4,10 +4,10 @@ import type { Update } from "telegraf/typings/core/types/typegram";
 export const logRequest = (ctx: Context<Update>, next: () => Promise<void>) => {
   let command = ctx.message && "text" in ctx.message ? ctx.message.text : 'N/A'
   if (command !== 'N/A') {
-    command = command !== 'N/A' ? command.slice(0, command.indexOf(' ')) : command;
+    command = command !== 'N/A' ? command.slice(0, command.indexOf(' ') + 1) : command;
   }
   const requestFrom = ctx.from?.username || ctx.from?.first_name || 'Unknown';
 
-  console.log(`Request received: From: ${requestFrom}, Chat ID: ${ctx.chat?.id}, Command: ${command[0] === '/' ? command : 'N/A'}`);
+  console.log(`Request received. From: ${requestFrom}, Chat ID: ${ctx.chat?.id}, Command: ${command[0] === '/' ? command : 'N/A'}`);
   next();
 };
